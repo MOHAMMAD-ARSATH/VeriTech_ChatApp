0807f99d60ae534c56f7b45878c224a31310c71b
Mohammad Arsath <92676635+MOHAMMAD-ARSATH@users.noreply.github.com>
2025-08-07 21:20:26 +0530

    LogOut.jsx

--- src/components/LogOut.jsx
+++ src/components/LogOut.jsx
@@ -1,13 +1,15 @@
-import React from "react";
 import { auth } from "../firebase";
 
 const style = {
-  button: `bg-gray-200 px-4 py-2 hover:bg-gray-100`,
+  button: `bg-gray-200 px-4 py-2 hover:bg-gray-100 rounded-xl shadow-md transition duration-300 ease-in-out font-semibold`,
 };
 
 const LogOut = () => {
   const signOut = () => {
-    auth.signOut(); 
+    const confirmLogout = window.confirm("Are you sure you want to logout?");
+    if (confirmLogout) {
+      auth.signOut();
+    }
   };
 
   return (
